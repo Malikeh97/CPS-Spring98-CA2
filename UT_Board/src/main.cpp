535d117bfc324cd8423339e686d137b8f869beb3
Malikeh97 <malikehehghaghi@gmail.com>
2019-02-22 00:00:58 +0330

    Comments updated!

--- UT_Board/src/main.cpp
+++ UT_Board/src/main.cpp
@@ -23,27 +23,29 @@ int ADXLAddress = 0x6E; // Device address in which is also included the 8th bit 
 
 byte getData(int reg){
   byte data ;
-  Wire.beginTransmission(ADXLAddress); // Begin transmission to the Sensor 
+  Wire.beginTransmission(ADXLAddress); // Begin transmission to the Sensor
   //Ask the particular registers for data
   Wire.write(reg);
   Wire.endTransmission(); // Ends the transmission and transmits the data from the two registers
-  
+
   Wire.requestFrom(ADXLAddress,1); // Request the transmitted two bytes from the two registers
-  
-  if(Wire.available()<=1) {  // 
-    data = Wire.read(); // Reads the data from the register   
+
+  if(Wire.available()<=1) {  //
+    data = Wire.read(); // Reads the data from the register
   }
   return data;
 }
 
+//X: convert 4byte data into float
 float calc_x_out() {
   u_x.b[0] = getData(X_Axis_Register_DATAX0);
   u_x.b[1] = getData(X_Axis_Register_DATAX1);
   u_x.b[2] = getData(X_Axis_Register_DATAX2);
-  u_x.b[3] = getData(X_Axis_Register_DATAX3); 
+  u_x.b[3] = getData(X_Axis_Register_DATAX3);
   return u_x.f;
 }
 
+//Y: convert 4byte data into float
 float calc_y_out() {
   u_y.b[0] = getData(Y_Axis_Register_DATAY0);
   u_y.b[1] = getData(Y_Axis_Register_DATAY1);
@@ -54,21 +56,21 @@ float calc_y_out() {
 
 
 void setup() {
-  Wire.begin(); // Initiate the Wire 
+  Wire.begin(); // Initiate the Wire
   Serial.begin(9600);
-  delay(100); 
+  delay(100);
   // Enable measurement
   Wire.beginTransmission(ADXLAddress);
   Wire.write(Power_Register);
   // Bit D3 High for measuring enable (0000 1000)
-  Wire.write(1);  
+  Wire.write(1);
   Wire.endTransmission();
 }
 
 void loop() {
   float x_out = calc_x_out();
   float y_out = calc_y_out();
-  
+
   float total = y_out*y_out + x_out*x_out;
   Serial.print(total);
   delay(100);
